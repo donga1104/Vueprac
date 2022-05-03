bf9ce985b6eaad07d0398b6c999b85db90bb4481
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-03 20:56:08 +0900

    v-forによるリストをレンダリング

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const appdata = {
   data() {
     return {
-     flag : true 
+      message : '※データをテーブル表示する',
+      items: [
+        {name:'taro', mail:'taro@mail', tel:'01-234-5678'},
+        {name:'hanako', mail:'hana@mail', tel:'99-234-5678'},
+        {name:'sachiko', mail:'sacchi@mail', tel:'01-222-5678'},
+        {name:'jiro', mail:'konto@mail', tel:'43-3232-5678'},
+      ]
     }
-  },
-  mounted() {
-    setInterval(()=>{
-      this.flag = !this.flag
-    }, 1000)
   }
 }
 
