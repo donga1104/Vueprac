b6d668ff5448702e46659a5190965168524f899c
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-03 21:44:13 +0900

    コンポーネントの作成
    helloコンポーネントを作成

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const appdata = {
   data() {
     return {
-      message : '※データをテーブル表示する',
-      // オブジェクトでv-for
-      items: [
-        {name: 'taro', mail: 'taro@mail', tel: '1234'},
-        {name: 'taro2', mail: 'taro2@mail', tel: '2222'},
-        {name: 'taro3', mail: 'taro3@mail', tel: '3333'},
-        {name: 'taro4', mail: 'taro4@mail', tel: '4444'},
-      ]
+      message : '※コンポーネントを表示する',
     }
   }
 }
 
 let app = Vue.createApp(appdata)
+
+// コンポーネントの作成
+app.component('hello', {
+  template : '<p class="alert alert-primary">Hello!</p>'
+})
+
 app.mount('#app')
