867fbad6c6034bac100cafd79bc3f62e6de21be9
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-05 17:07:24 +0900

    単位変換アプリの作成watch

--- vuedemy/todo/main.js
+++ vuedemy/todo/main.js
@@ -1,11 +1,19 @@
 const app = Vue.createApp({
     data:() => ({
-        message: 'hello,Vue.js'
+        km: 0,
+        m: 0
     }),
     watch: {
-        message: function(newValue, oldValue) {
-            console.log('new: %s, old: %s', newValue, oldValue)
+        km: function(value) {
+            console.log(value)
+            this.km = value
+            this.m = value * 1000
+        },
+        m: function(value) {
+            this.km = value / 1000
+            this.m = value
         }
+
     }
 })
 
