a3e4e2c7fe26cfa33e6aa570563a1b601fad1b83
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-05 20:21:55 +0900

    三項演算子で確認

--- vuedemy/todo/main.js
+++ vuedemy/todo/main.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
     data:() => ({
-        classObject: {
-            large: true,
-            'text-danger':true
-        }
+        largeClass: {
+            'large': true,
+            'bg-gray': true
+        },
+        dangerClass: {
+            'text-danger': true
+        },
+        isLarge: true // trueでlargeClassがバインドされる
     })
 
 })
