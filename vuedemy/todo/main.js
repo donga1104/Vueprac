59cd65fc3a5cbf7f18b977c56c88ff3b5104d7d8
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-05 15:51:42 +0900

    画面のちらつきを対策（v-cloak)
    ちらつき対策：インスタンスのコンパイルが終了するまで、該当要素をcssで非表示にする。 v-cloak

--- vuedemy/todo/main.js
+++ vuedemy/todo/main.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data:() => ({
-        // 通常xss対策でタグはそのまま表示されるが、v-htmlでタグを記述可能
-        message : 'hello <span style="color:red"> red </span>'
+        message : 'hello'
     })
 })
 
