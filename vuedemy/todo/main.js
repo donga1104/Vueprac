2772ff3769bc20038c580dc958f03e751947f370
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-05 15:37:01 +0900

    v-pre : コンパイル省略

--- vuedemy/todo/main.js
+++ vuedemy/todo/main.js
@@ -1,23 +1,11 @@
 const app = Vue.createApp({
     data:() => ({
-        newItem: '',
-        todos: []
+        message : 'hello'
     }),
     methods: {
-        addItem: function(event) {
-            if(this.newItem === '')return // 空文字は追加しない
-            let todo = {
-                item : this.newItem,
-                isDone : false
-            }
-            this.todos.push(todo)
-            this.newItem = ''   // 入力欄をクリアする
-        },
-        deleteItem: function(index) {
-            // console.log('delete is ' + index)    削除対象はindexで区別
-            this.todos.splice(index, 1)
+        clickHandler: function(event) {
+            this.message = this.message.split('').reverse().join('') // 文字列反転
         }
-
     }
 })
 
