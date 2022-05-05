9111e260915e569b9ade74376329ae8cd1971188
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-05 18:20:44 +0900

    qiita api で動作確認
    data,mounted,methods,watchの動作確認

--- vuedemy/todo/main.js
+++ vuedemy/todo/main.js
@@ -1,22 +1,42 @@
 const app = Vue.createApp({
     data:() => ({
-        colors: [
-            {name: 'red'},
-            {name: 'blue'},
-            {name: 'green'},
-        ]
+        items: null,
+        keyword: '',
+        message: ''
     }),
     watch: {
-        colors: {
-            handler: function(newValue, oldValue) {
-                console.log('update')
-            },
-            deep: true // ネストされた内容も監視する（console.log('update')が表示される
+        keyword: function(newKeyword, oldKeyword) {
+        this.message = "Waiting for you to stop typing..."
+        this.debouncedGetAnswer()   // debounce関数（lodashのユーティリティ）
         }
     },
+    mounted: function() {
+        // this.getAnswer() // debounce関数でリクエストを節約
+
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000) // 指定時間内に同じイベント発生：処理実行しない。
+    },
     methods: {
-        onClick: function(event) {
-            this.colors[1].name = 'white'
+        getAnswer: function() {
+            if(this.keyword === '') {
+                console.log('karamoji')
+                this.items = null
+                return
+            }
+
+            this.message = 'lading...'
+            const vm = this
+            const params = {page: 1, per_page: 20, query: this.keyword}
+            axios.get('https://qiita.com/api/v2/items', { params })
+                .then(function(response) {
+                    console.log(response)
+                    vm.items = response.data
+                })
+                .catch(function(error) {
+                    vm.message = 'Error' + error
+                })
+                .finally(function() {
+                    vm.message = ''
+                })
         }
     }
 })
