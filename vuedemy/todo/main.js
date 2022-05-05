6f21afb29850db3936a6a891c0c8b934dfb445f2
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-05 20:04:10 +0900

    v-bindで動的にスタイルを当てる

--- vuedemy/todo/main.js
+++ vuedemy/todo/main.js
@@ -1,44 +1,8 @@
 const app = Vue.createApp({
     data:() => ({
-        items: null,
-        keyword: '',
-        message: ''
-    }),
-    watch: {
-        keyword: function(newKeyword, oldKeyword) {
-        this.message = "Waiting for you to stop typing..."
-        this.debouncedGetAnswer()   // debounce関数（lodashのユーティリティ）
-        }
-    },
-    mounted: function() {
-        // this.getAnswer() // debounce関数でリクエストを節約
+        isLarge: true
+    })
 
-        this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000) // 指定時間内に同じイベント発生：処理実行しない。
-    },
-    methods: {
-        getAnswer: function() {
-            if(this.keyword === '') {
-                console.log('karamoji')
-                this.items = null
-                return
-            }
-
-            this.message = 'lading...'
-            const vm = this
-            const params = {page: 1, per_page: 20, query: this.keyword}
-            axios.get('https://qiita.com/api/v2/items', { params })
-                .then(function(response) {
-                    console.log(response)
-                    vm.items = response.data
-                })
-                .catch(function(error) {
-                    vm.message = 'Error' + error
-                })
-                .finally(function() {
-                    vm.message = ''
-                })
-        }
-    }
 })
 
 app.mount('#app')
