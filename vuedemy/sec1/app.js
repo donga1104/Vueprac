848b2fe9a26e8b7f5fe0d26d74959ab2e5d83e8f
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-05 11:44:48 +0900

    v-bindで入力欄の中身を記述

--- vuedemy/sec1/app.js
+++ vuedemy/sec1/app.js
@@ -2,13 +2,6 @@ const app = Vue.createApp({
     // options
     data: () => ({
         message : 'hello,Vue.js',
-        count: 100,
-        user: {
-            lastName: 'yoshida',
-            firstName: 'shun',
-            prefecture: 'Osaka'
-        },
-        colors: ['Red', 'Green', 'Blue']
     })
 }).mount('#app');
 // app.mount('#app');
