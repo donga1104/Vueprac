9228d227b6841177484e1d0e3f2fda03bd9e10e3
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-05-22 00:28:27 +0900

    publicPathの設定
    ホットリロードとの設定が出来ていない…

--- vuedemy/SFC/test/vue.config.js
+++ vuedemy/SFC/test/vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   // ホットリロードの設定
+//   transpileDependencies: true,
+//   // pathの変更 buildの時の設定
+//   // publicPath: ''
+// })
+
+module.exports = {
+  publicPath: ''
+}
