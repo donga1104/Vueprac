67c4e81a5c4af8aed4383c9cdc5618da1798c466
donga1104 <s1y1u1n1@yahoo.co.jp>
2022-06-05 23:29:50 +0900

    mapヘルパーの動作

--- vuedemy/vuex/vuex/src/components/HelloWorld.vue
+++ vuedemy/vuex/vuex/src/components/HelloWorld.vue
@@ -1,11 +1,12 @@
 <template>
 <div>
-  <button @click="increment">+</button>
+  <button @click="incrementAction">+</button>
   <button @click="addCount">+10</button>
 </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
   name: 'HelloWorld',
   props: {
@@ -25,14 +26,24 @@ export default {
     // ----- 直接mutatationを呼び出す ----- //
 
     // Actionを呼び出す
-    increment(){
-      this.$store.dispatch('incrementAction')
-    },
-    addCount() {
-      this.$store.dispatch('addCountAction', {
-        value:10
+    // increment(){
+    //   this.$store.dispatch('incrementAction')
+    // },
+      // ----- mapヘルパーで記述し直す ------
+      ...mapActions([('incrementAction', 'addCountAction')]),
+    // addCount() {
+    //   this.$store.dispatch('addCountAction', {
+    //     value:10
+    //   })
+    // },
+    
+    // addCountActionは...mapActionsで存在するので、value:10を渡してあげる
+    addCount(){
+      this.addCountAction({
+        value: 10
       })
     }
+
   }
 }
 </script>
